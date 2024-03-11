508816101bb41b7138181b6947ddbec48f9cdc74
Xavi-Arnau <chavier28@gmail.com>
2024-03-11 17:19:45 +0100

    tic tac toe

--- src/App.js
+++ src/App.js
@@ -10,6 +10,7 @@ import RandomColor from "./components/random-color";
 import ScrollIndicator from "./components/scroll-indicator";
 import SearchAutocomplete from "./components/search-autocomplete";
 import StarRating from "./components/star-rating";
+import TicTacToe from "./components/tic-tac-toe";
 import TreeView from "./components/tree-view";
 import menus from "./components/tree-view/data";
 
@@ -32,7 +33,8 @@ function App() {
       {/*<TabTest />*/}
       {/*<ModalTest />*/}
       {/*<GithubProfileFinder />*/}
-      <SearchAutocomplete />
+      {/*<SearchAutocomplete />*/}
+      <TicTacToe />
     </div>
   );
 }
